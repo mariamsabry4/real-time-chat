8a91fe83befb1b1bcc260778875fc321d4953f9e
Marcy <marsy2126@gmail.com>
2026-01-03 17:07:14 +0200

    Add initial routes and page components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,25 @@
+import { Routes, Route } from "react-router-dom";
+import SignUpPage from "./pages/SignUpPage";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/loginPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <>
-      <h1 className="text-red-800">Hello, Chatty!</h1>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-
-export default App
+export default App;
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            heeeeeeeeelo babies
+            
+        </div>
+    );
+}
+
+export default HomePage;
