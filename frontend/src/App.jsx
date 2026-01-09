05b53d2842dd895dd8114a8ccbeff0e1684f33b1
Marcy <marsy2126@gmail.com>
2026-01-09 23:15:35 +0200

    validate authUser

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,24 +7,33 @@ import ProfilePage from "./pages/ProfilePage";
 import Navbar from "./components/Navbar";
 import { useAuthStore } from "./store/useAuthStore";
 import { useEffect } from "react";
+import {Loader} from "lucide-react";
+import { Navigate } from 'react-router-dom';
+
 
 function App() {
-  const {authUser, checkAuth} = useAuthStore()
+  const {authUser, checkAuth, isCheckingAuth} = useAuthStore()
   useEffect(()=>{
 checkAuth()
   },[checkAuth]);
   console.log("authUser in App.jsx", authUser);
+
+  if(isCheckingAuth && !authUser )return(
+    <div className ="flex items-center justify-center h-screen">
+      <Loader className ="size-10 animate-spin" />
+    </div>
+  );
 
   return (
     <>
       <Navbar />
 
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={authUser? <HomePage /> : <Navigate to= "/login" />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to= "/" />} />
         <Route path="/settings" element={<SettingsPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/profile" element={authUser? <ProfilePage /> : <Navigate to= "/login" />} />
       </Routes>
     </>
   );
